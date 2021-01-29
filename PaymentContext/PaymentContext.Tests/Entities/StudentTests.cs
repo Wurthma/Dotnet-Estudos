1842cc930ed262d6baa1b62742c391e848426b3c
George Henrique Wurthmann <gemakai@gmail.com>
2021-01-28 23:39:09 -0300

    Organização das entidades e aplicação de conceitos de OO e SOLID

---
+++ PaymentContext/PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,14 @@
+using PaymentContext.Domain.Entities;
+using Xunit;
+
+namespace PaymentContext.Tests.Entities
+{
+    public class StudentTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            //var student = new Student();
+        }
+    }
+}
